caf89def937113e7ce831b4699e7d1c029e0e05a
Stuart <gravatar@strudel.org.uk>
2015-12-09 23:42:57 +0000

    Add processed version to see if this is visible

---
+++ lang/convert.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+$xml = $ARGV[0];
+
+open(FILE,$xml);
+@xmllines = <FILE>;
+close(FILE);
+
+open(FILE,"en.txt");
+@lines = <FILE>;
+close(FILE);
+
+%dict;
+
+foreach $line (@xmllines){
+	if($line =~ /<([^\>]*)>([^\<]*)<\/\1>/){
+		$dict{$1} = $2;
+	}
+}
+
+foreach $line (@lines){
+	if($line =~ /^([^\:]*)\:([\s\t]*)\"(.*)\"$/){
+		print "$1\:$2\"$dict{$1}\"\n";
+	}
+}
